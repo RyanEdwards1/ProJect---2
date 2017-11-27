c78a11b73c92cf1072039fddc5fa5c237c51c9b8
RyanEdwards1 <31927415+RyanEdwards1@users.noreply.github.com>
2017-11-27 13:07:56 +0000

    Update Code.h

--- Code.h
+++ Code.h
@@ -1,10 +1,10 @@
-#include <cstdlib> // Loads required libraries
+#include <cstdlib> // Loads libraries
 #include <time.h>  // --
 #include <iostream> // --
 
 using namespace std; // Makes functions with the same names within different libraries distinguishable
 
-int PickCard() { // The function that contains the code that generates a random number between 1 and 13
+int PickCard() { // The function that generates a random number between 1 and 13
   
    
   int x; // Placehodler int to hold the card value
@@ -17,27 +17,27 @@ int PickCard() { // The function that contains the code that generates a random 
 
 
 
-main() { // Main function that is the first to be performed
+main() { // Main function
   
-	int cash = 0; // Makes the cash variable and sets 
+	int cash = 0; // Makes the cash variable
   
 	CorrectGuess: // Marker to go to later so that the cash variable doesn't get reset to 0
   
-	srand((unsigned)time(NULL)); // Seeds the random number generator based on the current time
+	srand((unsigned)time(NULL)); // Seeds the random number generator
   
-    int card1 = PickCard(); // Declares a new variable called card1 with a value decided by the random number generator
+    	int card1 = PickCard(); // Declares a new variable called card1 with a value decided by the random number generator
   
-    cout << "Your number is" << "\n"; // The following lines print the text in the command line giving the player information and instructions
+    	cout << "Your number is" << "\n"; // The following lines print the text in the command line
   
-    cout << card1 << "\n"; // Prints value of card1
+    	cout << card1 << "\n"; // Prints value of card1
     
-    cout << "Higher or Lower? 1 for Higher, 2 for Lower" << "\n"; // --
+    	cout << "Higher or Lower? 1 for Higher, 2 for Lower" << "\n"; // --
     
-    int card2 = PickCard(); // Declares a new variable called card2 with a value decided by the random number generator
+    	int card2 = PickCard(); // Declares a new variable called card2 with a value decided by the random number generator
   
-    int guess; // Declares a new variable to hold the players guess
+    	int guess; // Declares a new variable to hold the players guess
 
-    cin >> guess; // Whatever the player inputs into the command line is held as the value to the variable guess
+    	cin >> guess; // Whatever the player inputs into the command line is held as the value to the variable guess
     
 		if ( guess == 1 ){ // This checks to see what the players guess was, the value of '1' means the player guessd higher
 			if ( card2 > card1 ){ // This checks if the players guess was correct
@@ -46,21 +46,22 @@ main() { // Main function that is the first to be performed
 				cout << "You guessed higher. The number was: " << card2 << ". Well Done!" "\n" "\n"; // Prints text to let the player know that their guess was correct
 
 				goto CorrectGuess; // Sends the program back to the 'CorrectGuess' marker
-				}
-					else cout << "Game over, the number was: " << card2 << "\n" << "You finished the game with " << cash << " Dollars" "\n" "\n"; // If the player's guess was wrong, prints game over text and displays how much cash they made
-					}
-    
-		if ( guess == 2 ){ // This checks to see if the players guess was lower, value of 2
-			if ( card2 < card1 ){ // This checks if the players guess was correct
-				cash += 50; // adds 50 to the value of the variable 'cash'
 				
-				cout << "You guessed lower. The number was: " << card2 << ". Well Done!" "\n" "\n"; // Prints text to let the player know that their guess was correct
-					
-				goto CorrectGuess; // Sends the program back to the 'CorrectGuess' marker
 				}
 					else cout << "Game over, the number was: " << card2 << "\n" << "You finished the game with " << cash << " Dollars" "\n" "\n"; // If the player's guess was wrong, prints game over text and displays how much cash they made
-					}
+				}
+    
+					if ( guess == 2 ){ // This checks to see if the players guess was lower, value of 2
+						if ( card2 < card1 ){ // This checks if the players guess was correct
+							cash += 50; // adds 50 to the value of the variable 'cash'
+				
+							cout << "You guessed lower. The number was: " << card2 << ". Well Done!" "\n" "\n"; // Prints text to let the player know that their guess was correct
+					
+							goto CorrectGuess; // Sends the program back to the 'CorrectGuess' marker
+					
+							}
+								else cout << "Game over, the number was: " << card2 << "\n" << "You finished the game with " << cash << " Dollars" "\n" "\n"; // If the player's guess was wrong, prints game over text and displays how much cash they made
+							}
 
 	  
 }
-
